88ca3472ea5124244f563492c039b88e3a1556ae
shinexus <shinexus@gmail.com>
2016-12-15 18:38:26 +0800

    新文件测试

---
+++ madns/FinderEyeForm.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： shinexus@gmail.com
+ * 日期: 2016/12/15
+ * 时间: 18:31
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace madns
+{
+	public partial class FinderEyeForm : Form
+	{
+		public FinderEyeForm()
+		{
+			
+		}
+	}
+}
+	
